15e184e4c7fa4b79decb97f2efec169b39e8cd05
zachmolony <31472522+zachmolony@users.noreply.github.com>
2019-03-10 23:47:20 +0000

    added particle as header bg

--- main.js
+++ main.js
@@ -15,3 +15,16 @@ $(".info button").click(function(){
   },750)
   
  })
+
+var myNav = document.getElementById('mynav');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 500 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
